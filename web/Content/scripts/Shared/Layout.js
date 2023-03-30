220253410aa3a164ab324f5a641d3c550f3b3025
Mauricio <mauricio@rodosoft.com.br>
2023-03-30 10:55:01 -0300

    Ajustado conflitos

--- web/Content/scripts/Shared/Layout.js
+++ web/Content/scripts/Shared/Layout.js
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-carregarLayout();
-
-function carregarLayout() {
-    var wrapper = document.createElement('div');
-    wrapper.innerHTML = '<div></div>';
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'Views/Shared/Layout.html', true);
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            wrapper.firstChild.innerHTML = xhr.responseText;
-
-            var layoutContent = wrapper.firstChild.querySelector('main');
-            document.body.insertBefore(layoutContent, document.getElementById('conteudo'));
-
-            var links = wrapper.firstChild.querySelectorAll('link');
-            var scripts = wrapper.firstChild.querySelectorAll('script');
-            
-            var head = document.head;
-            for (var i = 0; i < links.length; i++) {
-                head.appendChild(links[i].cloneNode(true));
-            }
-            for (var j = 0; j < scripts.length; j++) {
-                head.appendChild(scripts[j].cloneNode(true));
-            }
-        }
-    };
-    xhr.send();
-=======
 carregarLayout();
 
 function carregarLayout() {
@@ -59,5 +29,4 @@ function carregarLayout() {
         }
     };
     xhr.send();
->>>>>>> e76f7e9 (Ajustado forma de carregar layout.html no index)
 };
