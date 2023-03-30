f4dfd8c8666093c865d7f1f20963e0dba4d76d22
Canabarro, Pedro (ESI) <pedro.canabarro@adp.com>
2023-03-30 08:47:56 -0300

    importing api

--- web/Content/scripts/Shared/Layout.js
+++ web/Content/scripts/Shared/Layout.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 carregarLayout();
 
 function carregarLayout() {
@@ -26,4 +27,37 @@ function carregarLayout() {
         }
     };
     xhr.send();
+=======
+carregarLayout();
+
+function carregarLayout() {
+    var wrapper = document.createElement('div');
+    wrapper.innerHTML = '<div></div>';
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'Views/Shared/Layout.html', true);
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            wrapper.firstChild.innerHTML = xhr.responseText;
+
+            var layoutContentMain = wrapper.firstChild.querySelector('header');
+            document.body.insertBefore(layoutContentMain, document.getElementById('conteudo'));
+
+            var layoutContentFooter = wrapper.firstChild.querySelector('footer');
+            document.body.insertBefore(layoutContentFooter, document.querySelector('footer'));
+
+            var links = wrapper.firstChild.querySelectorAll('link');
+            var scripts = wrapper.firstChild.querySelectorAll('script');
+            
+            var head = document.head;
+            for (var i = 0; i < links.length; i++) {
+                head.appendChild(links[i].cloneNode(true));
+            }
+            for (var j = 0; j < scripts.length; j++) {
+                head.appendChild(scripts[j].cloneNode(true));
+            }
+        }
+    };
+    xhr.send();
+>>>>>>> e76f7e9 (Ajustado forma de carregar layout.html no index)
 };
