935bd4710e2779da1ad8295575904225bfeed911
pcanabarro <pedroh.canabarro@gmail.com>
2023-03-31 14:45:10 -0300

    code organization

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const Router = require('./api/src/routes/router.js')
+require('dotenv').config()
+
+const app = express();
+
+const PORT = process.env.SERVER_PORT;
+
+app.use(Router.getRouter());
+
+app.listen(PORT, () =>
+    console.log('Server running on port', PORT)
+);
